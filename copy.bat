44f7e64994fb42261202371a5f661c799342e39e
Manhcuongz1 <cuong15122002@gmail.com>
2022-01-06 11:53:50 +0700

    cuong

---
+++ copy.bat
@@ -0,0 +1,4 @@
+del "%cd%\Arena\p1.exe"
+del "%cd%\Arena\p2.exe"
+copy "%cd%\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "%cd%\Arena\p1.exe"
+copy "%cd%\Bots\C++\C++Template\2015_Debug\AI_Template.exe" "%cd%\Arena\p2.exe"
